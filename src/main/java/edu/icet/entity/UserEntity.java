4b549cb63bfbc808e2c0a4b686457de86a54a353
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-22 22:03:50 +0530

    Update DTO

--- src/main/java/edu/icet/entity/UserEntity.java
+++ src/main/java/edu/icet/entity/UserEntity.java
@@ -1,4 +1,19 @@
 package edu.icet.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
 }
