5301ac838fb409719af5b63bf08fb66868b0f8ed
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-23 19:19:11 +0530

    Update

--- src/main/java/edu/icet/repository/InventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.repository;
-
-import edu.icet.entity.InventoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
-}
