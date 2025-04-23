d103629c1683cac5bab415b96d257842e2da614f
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-23 18:39:06 +0530

    Inventory CRUD

--- src/main/java/edu/icet/repository/InventoryRepository.java
+++ src/main/java/edu/icet/repository/InventoryRepository.java
@@ -1,9 +1,9 @@
 package edu.icet.repository;
 
-import edu.icet.entity.UserEntity;
+import edu.icet.entity.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
 }
